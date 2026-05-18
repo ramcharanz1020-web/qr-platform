7ecfcaff41315f7e595446af75d4c5403503d652
unknown <ramcharanz1020@gmail.com>
2026-05-18 10:39:44 +0530

    updated production api

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,19 @@
 const mysql = require("mysql2");
 
-const pool = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
-module.exports = pool.promise();
+db.connect((err) => {
+  if (err) {
+    console.log("Database connection failed:", err);
+  } else {
+    console.log("Database connected");
+  }
+});
+
+module.exports = db;
